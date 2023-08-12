accbb9ea87b18af4f4d046f661601be331dcbfdb
Stuart Bloxham <s.bloxham@hotmail.co.uk>
2023-08-12 22:47:01 +0100

    object fit contain

--- components/NewHero.jsx
+++ components/NewHero.jsx
@@ -13,7 +13,7 @@ const NewHero = () => {
           src={blur}
           alt="blur"
           fill
-          style={{ objectFit: "cover", position: "absolute" }}
+          style={{ objectFit: "contain", position: "absolute" }}
           quality={100}
           sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
           priority
