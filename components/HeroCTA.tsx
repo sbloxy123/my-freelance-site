4a71a174856997dc81df5cc832738e22c72b7376
Stuart Bloxham <s.bloxham@hotmail.co.uk>
2023-08-13 15:02:58 +0100

    btn link fix on hero

--- components/HeroCTA.tsx
+++ components/HeroCTA.tsx
@@ -15,11 +15,13 @@ const HeroCTA = () => {
         }}
         viewport={{ once: true }}
       >
-        <div className="py-3 md:px-15 w-full text-center mb-6 rounded-sm text-lg bg-theme_green-900 hover:bg-theme_black-900 hover:text-theme_green-900 capitalize border border-theme_black-900 hover:border-2 hover:border-theme_green-900 ease-in-out duration-300 cursor-pointer">
-          <Link type="button" href="/projects/">
-            Projects
-          </Link>
-        </div>
+        <Link
+          type="button"
+          className="py-3 md:px-15 w-full text-center mb-6 rounded-sm text-lg bg-theme_green-900 hover:bg-theme_black-900 hover:text-theme_green-900 capitalize border border-theme_black-900 hover:border-2 hover:border-theme_green-900 ease-in-out duration-300 cursor-pointer"
+          href="/projects/"
+        >
+          Projects
+        </Link>
       </motion.div>
       <motion.div
         className="md:w-[50%]"
@@ -31,11 +33,13 @@ const HeroCTA = () => {
         }}
         viewport={{ once: true }}
       >
-        <div className="py-3 md:px-15 capitalize w-full text-center mb-6 rounded-sm border-2 border-theme_dark_orange-900 text-theme_dark_orange-900 font-semibold text-lg hover:text-theme_black-900 hover:bg-theme_light_orange-900 ease-in-out duration-300 cursor-pointer">
-          <Link type="button" href="/information/">
-            What is NEXT.jS?
-          </Link>
-        </div>
+        <Link
+          type="button"
+          className="py-3 md:px-15 capitalize w-full text-center mb-6 rounded-sm border-2 border-theme_dark_orange-900 text-theme_dark_orange-900 font-semibold text-lg hover:text-theme_black-900 hover:bg-theme_light_orange-900 ease-in-out duration-300 cursor-pointer"
+          href="/information/"
+        >
+          What is NEXT.jS?
+        </Link>
       </motion.div>
     </div>
   );
